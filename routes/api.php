e735b8514116d97d9b9090cb833992013e04b915
Ali Esmaeel <ali.esma3iel@gmail.com>
2026-09-04 12:27:28 +0100

    fix

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,7 @@ Route::get('/offplan-projects/{slug}', [\App\Http\Controllers\Api\HomeController
 Route::get('/global-project/{name}', [\App\Http\Controllers\Api\HomeController::class, 'globalProjectDetails']);
 Route::get('/popular-areas/{slug}', [\App\Http\Controllers\Api\HomeController::class, 'popularAreaDetails']);
 Route::get('/property/{slug}', [\App\Http\Controllers\Api\HomeController::class, 'propertyDetails']);
-Route::post('/career_apply/{careerId}', [CareerController::class, 'apply'])->name('career_apply');
+Route::post('/career_apply/{careerId}', [CareerController::class, 'apply'])->name('api_career_apply');
 Route::get('/careers', [CareerController::class, 'careerListApi'])->name('api_career_list_api');
 Route::get('/real-estate-guides', [\App\Http\Controllers\Api\HomeController::class, 'realEstateGuides']);
 Route::get('/download-guide/{id}', [\App\Http\Controllers\Api\HomeController::class, 'downloadGuide']);
